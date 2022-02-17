48cab8f26602f6a26dcabb52be980022fc8423a0
dariok87 <dkemenovic@gmail.com>
2022-02-17 19:50:27 +0100

    Create primitive component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there</div>
+  return (
+    <div className='ui conatiner comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' />
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Sam
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 5:00PM</span>
+          </div>
+          <div className='text'>Nice blog post</div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
-ReactDOM.render(<App />, document.querySelector('root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
