8429c763de5b304530efb97f73e016c5a8e2b17a
dariok87 <dkemenovic@gmail.com>
2022-02-17 20:20:24 +0100

    Duplicate single component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,35 @@ import faker from 'faker';
 
 const App = () => {
   return (
-    <div className='ui conatiner comments'>
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' src={faker.image.image()}/>
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Sam
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 5:00PM</span>
+          </div>
+          <div className='text'>Nice blog post</div>
+        </div>
+      </div>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' src={faker.image.image()}/>
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Sam
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 5:00PM</span>
+          </div>
+          <div className='text'>Nice blog post</div>
+        </div>
+      </div>
       <div className='comment'>
         <a href='/' className='avatar'>
           <img alt='avatar' src={faker.image.image()}/>
