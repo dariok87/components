ebffd674ab39453af61123eaac29e34ee02be9b0
dariok87 <dkemenovic@gmail.com>
2022-02-19 09:56:43 +0100

    Reformat code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import faker from 'faker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail'
+import faker from 'faker'
 
 const App = () => {
   return (
-    <div className='ui container comments'>
-      <CommentDetail author='Sam' timeAgo='Today at 5:00' critic='bunch of lies' avatar={faker.image.image()} />
-      <CommentDetail author='John' timeAgo='Today at 4:50' critic='nice post' />
-      <CommentDetail author='Jane' timeAgo='Today at 3:28' critic='totally agree' />
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 5:00"
+        critic="bunch of lies"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail author="John" timeAgo="Today at 4:50" critic="nice post" />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 3:28"
+        critic="totally agree"
+      />
     </div>
   )
-};
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'))
